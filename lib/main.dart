efff24126c52dadb6442880297e361218a2d3d86
ranjan.k <ranjan.k@intense.in>
2022-02-25 16:12:36 +0530

    day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,6 +14,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     double days = 30;
 
-    return MaterialApp(home: Homepage());
+    return MaterialApp(
+      //home: Homepage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {
+        "/": ((context) => LoginPage()),
+        "/home": ((context) => Homepage())
+      },
+      initialRoute: "/home",
+    );
   }
 }
